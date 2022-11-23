8201d6bdfb49b895245ffa784ad4058fbc2c5966
Mishell Alexandra Yánez Tufiño <mayanez10@espe.edu.ec>
2022-11-22 22:51:58 -0500

    create class Client

---
+++ 06-Code/WAD_RIOT_ClientServer_InvoicingSystem/src/main/java/Model/Client.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.web;
+
+/**
+ *
+ * @author mishe
+ */
+public class Client {
+    String id;
+    String name;
+    String address;
+    String cellphone;
+    String email;
+
+    public Client() {
+    }
+
+    public Client(String id, String name, String address, String cellphone, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.cellphone = cellphone;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+}

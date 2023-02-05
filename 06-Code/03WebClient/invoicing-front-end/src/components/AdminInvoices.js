704bb25ee87a52929e6f8700a77aa46f0a87550d
Mishell Alexandra Yánez Tufiño <mayanez10@espe.edu.ec>
2023-02-05 18:32:29 -0500

    Update AdminInvoices

--- 06-Code/03WebClient/invoicing-front-end/src/components/AdminInvoices.js
+++ 06-Code/03WebClient/invoicing-front-end/src/components/AdminInvoices.js
@@ -1,10 +1,56 @@
+// Author: Mishell Yánez
+// Based on: https://www.youtube.com/watch?v=nI8PYZNFtac & for React18 error of useEffect https://www.youtube.com/watch?v=81faZzp18NM
+import { useState, useEffect, useRef } from 'react';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
 
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
+
 const AdminInvoices = () => {
+    const [invoices, setInvoices] = useState();
+    const axiosPrivate = useAxiosPrivate();
+    // For react18 changed useEffect to run Twice
+    const effectRan = useRef(false)
+
+    const [isLoading, setLoading ] = useState(true);
     const navigate = useNavigate();
+
+    // Get data
+    useEffect(()=>{
+        if(effectRan.current===false){
+            const controller = new AbortController(); // To cancel request if component unmounts
+            
+            const getInvoices = async () => {
+                try{
+                    const response = await axiosPrivate.get('/restaurant/invoices',{
+                        
+                    });
+                    console.log(response.data)
+
+                    setInvoices(response.data.map(invoice => (
+                        {
+                            id: Math.random() * (100000 - 1)+1,
+                            clientIDCard: invoice.clientIDCard,
+                            orderToGo: invoice.orderToGo,
+                            invoiceDate: invoice.invoiceDate,
+                            totalInvoice: invoice.totalInvoice,
+                            paymentState: invoice.paymentState
+                        }
+                    )));
+                    setLoading(false);
+                } catch (error){
+                    console.log(error);
+                }
+            }
+            getInvoices();
+            return () => {
+                controller.abort();
+                effectRan.current = true;
+            }
+        }
+    }, [])
 
     // DataGrid
     const ModifyButton = () => {
@@ -22,14 +68,18 @@ const AdminInvoices = () => {
 
     const columns = [
         { field:'id', hide: true },
-        { field:'clientIDCard', headerName:'Cliente', width: 170},
-        { field:'orderToGo', headerName:'¿Para Llevar?', width: 200 },
-        { field:'invoiceDate', headerName:'Fecha', width: 300 },
-        { field:'totalInvoice', headerName:'Total', width: 200 },
-        { field:'paymentState', headerName:'Estado de Pago', width: 300 },
+        { field:'clientIDCard', headerName:'Cliente', width: 200},
+        { field:'orderToGo', headerName:'¿Para Llevar?', width: 150 },
+        { field:'invoiceDate', headerName:'Fecha', width: 170 },
+        { field:'totalInvoice', headerName:'Total', width: 150 },
+        { field:'paymentState', headerName:'Estado de Pago', width: 180 },
         { field:'modifyButton', headerName:'Modificar', width:150, renderCell: () => <ModifyButton/> },
         { field:'deleteButton', headerName:'Borrar', width:150, renderCell: () => <DeleteButton/> }
     ];
+
+    if (isLoading) {
+        return <><h1>Cargando...</h1></>
+    }
 
     return (
         <>
@@ -38,7 +88,7 @@ const AdminInvoices = () => {
             <Button onClick={() => navigate("/newInvoice")} sx={{background:'#009F6B', color:"#fff", "&:hover": {color: '#fff', background: '#32CD32'}, borderRadius: '0.5rem'}}>Crear Nueva Factura</Button>
             <br/>
             <Grid container justifyContent="center" sx={{ height: 500, width: '72%'}}> 
-                <DataGrid columns={columns} rows={[]} className="dataGrid" sx={{alignSelf:"center"}}/>
+                <DataGrid columns={columns} rows={ invoices} className="dataGrid" sx={{alignSelf:"center"}}/>
             </Grid>
             <br/>
             <Button onClick={() => {navigate(-1)}} sx={{background:'rgb(144,30,56)', color:"#fff", "&:hover": {color: '#fff', background: '#DA2C43'}, borderRadius: '0.5rem'}}>Volver</Button>

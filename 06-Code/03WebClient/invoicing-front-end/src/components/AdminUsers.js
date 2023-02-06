81efda66da6e480f669f0985341773bece4679a6
Mishell Alexandra Yánez Tufiño <mayanez10@espe.edu.ec>
2023-02-05 19:00:17 -0500

    Update AdminUser

--- 06-Code/03WebClient/invoicing-front-end/src/components/AdminUsers.js
+++ 06-Code/03WebClient/invoicing-front-end/src/components/AdminUsers.js
@@ -2,9 +2,56 @@ import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import { DataGrid } from '@mui/x-data-grid';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 
 const AdminUsers = () => {
+    const [ users, setUsers ] = useState();
+    const axiosPrivate = useAxiosPrivate();
+    // For react18 changed useEffect to run Twice
+    const effectRan = useRef(false)
+
+    const [isLoading, setLoading ] = useState(true);
+
     const navigate = useNavigate();
+
+    // Get data
+    useEffect(() => {
+    
+        if (effectRan.current === false){
+            const controller = new AbortController(); // To Cancel request if component unmounts
+
+            const getUsers = async () => {
+                try {
+                    const response = await axiosPrivate.get('/restaurant/users', {
+                        //signal: controller.signal
+                    });
+                    console.log(response.data)
+                    // If Component isMounted, set data and map it for rows DataGrid
+                    setUsers(response.data.map(user => (
+                        {
+                            id: Math.random() * (100000 - 1) + 1,
+                            fullName: user.fullName,
+                            email: user.email,
+                            username: user.username
+                        }
+                    )));
+                    
+                    setLoading(false);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+
+            getUsers();
+            // Cleanup function
+            return () => {
+                controller.abort();
+                effectRan.current = true;
+            }
+        }
+        
+    }, []) 
 
     // DataGrid
     const ModifyButton = () => {
@@ -22,12 +69,16 @@ const AdminUsers = () => {
 
     const columns = [
         { field:'id', hide: true },
-        { field:'fullName', headerName:'Nombre', width: 170},
-        { field:'email', headerName:'Email', width: 200 },
-        { field:'username', headerName:'Usuario', width: 300 },
+        { field:'fullName', headerName:'Nombre', width: 300},
+        { field:'email', headerName:'Email', width: 350 },
+        { field:'username', headerName:'Usuario', width: 150 },
         { field:'modifyButton', headerName:'Modificar', width:150, renderCell: () => <ModifyButton/> },
         { field:'deleteButton', headerName:'Borrar', width:150, renderCell: () => <DeleteButton/> }
     ];
+
+    if (isLoading) {
+        return <><h1>Cargando...</h1></>
+    }
 
     return (
         <>
@@ -36,7 +87,7 @@ const AdminUsers = () => {
             <Button onClick={() => navigate("/newUser")} sx={{background:'#009F6B', color:"#fff", "&:hover": {color: '#fff', background: '#32CD32'}, borderRadius: '0.5rem'}}>Crear Nuevo Usuario</Button>
             <br/>
             <Grid container justifyContent="center" sx={{ height: 500, width: '60%'}}> 
-                <DataGrid columns={columns} rows={[]} className="dataGrid" sx={{alignSelf:"center"}}/>
+                <DataGrid columns={columns} rows={users} className="dataGrid" sx={{alignSelf:"center"}}/>
             </Grid>
             <br/>
             <Button onClick={() => {navigate(-1)}} sx={{background:'rgb(144,30,56)', color:"#fff", "&:hover": {color: '#fff', background: '#DA2C43'}, borderRadius: '0.5rem'}}>Volver</Button>

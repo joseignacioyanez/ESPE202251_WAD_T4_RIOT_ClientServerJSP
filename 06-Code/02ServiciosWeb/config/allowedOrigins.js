b8dd45ce02532f27fcf97fac5318bdb48f0c3599
José Ignacio Yánez <joseignacioyanez@hotmail.com>
2023-02-08 23:05:51 -0500

    Add Allowed Origin for CORS Services the IP of the Front End

--- 06-Code/02ServiciosWeb/config/allowedOrigins.js
+++ 06-Code/02ServiciosWeb/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://www.google.com',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://34.105.52.36:3002'
 ];
 
 module.exports = allowedOrigins;
